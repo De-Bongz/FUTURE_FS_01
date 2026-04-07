83c68d3d1ecc667670038cb6959370b8e14f1e79
De-Bongz <bonganimaluleka572@gmail.com>
2026-04-07 15:56:04 +0200

    Upgraded the design and added information, including images

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio Ready 🚀");
+function showSection(sectionId, btn){
+    const cards = document.querySelectorAll(".card");
+    const buttons = document.querySelectorAll(".top-nav button");
+
+    cards.forEach(card => {
+        card.classList.remove("active");
+    });
+    buttons.forEach(button => button.classList.remove("active-btn"));
+
+    document.getElementById(sectionId).classList.add("active");
+    btn.classList.add(active-btn);
+}
