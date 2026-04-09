333eb0c9963220cd062ca35427c7f0ec8c745ef9
De-Bongz <bonganimaluleka572@gmail.com>
2026-04-09 12:49:34 +0200

    added backend for the contacts form to receive email on gmail.

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Email setup
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "bonganimaluleka572@gmail.com",
+        pass: "eqng umgk ihub xssa"
+    }
+});
+
+//Testing route
+app.get("/",(req, res) => {
+    res.send("Backend is running");
+});
+
+//contact route
+app.post("/contact",async (req, res) => {
+    const{ name, email, message } = req.body;
+
+    try{
+        const mailOptions = {
+      from: email,
+      to: "bonganimaluleka572@gmail.com",
+      subject: "Message from the portfolio web.",
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+      `
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.json({ success: true, message: "Message sent successfully 🚀" });
+
+    }   
+    catch (error) {
+        console.error(error);
+        res.json({ success: false, message: "Error sending message ❌" });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+});
+   
--- frontend/script.js
+++ frontend/script.js
@@ -10,3 +10,22 @@ function showSection(sectionId, btn){
     document.getElementById(sectionId).classList.add("active");
     btn.classList.add(active-btn);
 }
+
+async function sendMessage() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, email, message })
+    });
+
+    const data = await response.json();
+    alert(data.message);
+        
+    
+}
